d8c499298e77eaace5cd8f85b51731815b6c5f33
josedgregorio07 <josedgregorio07@gmail.com>
2020-03-28 17:44:26 -0600

    se corrigieron las migraciones para agregar las relaciones

--- database/migrations/2020_03_28_145904_add_relations_prepagas_table.php
+++ database/migrations/2020_03_28_145904_add_relations_prepagas_table.php
@@ -4,11 +4,12 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddRelationsHistoriasclinicasTable extends Migration
+class AddRelationsPrepagasTable extends Migration
 {
     public function up()
     {
-        Schema::table('historiasclinicas', function(Blueprint $table) {
+
+        Schema::table('prepagas', function (Blueprint $table) {
             $table->integer('paciente_id')->unsigned()->nullable()->change();
             $table->foreign('paciente_id')->references('id')->on('pacientes');
         });
@@ -16,7 +17,7 @@ class AddRelationsHistoriasclinicasTable extends Migration
 
     public function down()
     {
-        Schema::table('historiasclinicas', function (Blueprint $table) {
+        Schema::table('prepagas', function (Blueprint $table) {
             $table->dropForeign(['paciente_id']);
         });
     }
